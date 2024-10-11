5eb61967091e82881993bcad29db68e65d89f321
tanner-ray-martin <tanner.ray.martin.123@gmail.com>
2024-10-11 16:35:27 -0500

    Data Cleaner and Sorter Fixes #6

---
+++ src/fuel_bill_automation/configs/constants.py
@@ -0,0 +1,30 @@
+import os
+
+# DIRECTORIES
+VEHICLE_MANAGEMENT_DIRECTORY = r"L:\Rollout\Vehicle Management - SC"
+BEST_PASS_DIRECTORY = os.path.join(VEHICLE_MANAGEMENT_DIRECTORY, "Best Pass")
+BEST_PASS_STATEMENTS_DIRECTORY = os.path.join(
+    BEST_PASS_DIRECTORY, "Statements - Invoices"
+)
+FUEL_BILL_DIRECTORY = os.path.join(VEHICLE_MANAGEMENT_DIRECTORY, "Fuel Bills")
+
+## FUEL BILL STUFF
+FUEL_CHARGES_SHEET = r"L:\Rollout\Vehicle Management - SC\Fuel Bills\2024\08 - August 2024\08 - Aust 2024 Fuel Charges.xlsx"
+FUEL_BILL_SHEET = r"L:\Rollout\Vehicle Management - SC\Fuel Bills\2024\08 - August 2024\08 August 2024 Fuel Bill.xlsm"
+FUEL_BILL_SORTER_SHEET = (
+    r"L:\Rollout\Vehicle Management - SC\Fuel Bills\Fuel Bill Sorter MASTER.xlsm"
+)
+
+# VEHICLE_STUFF
+VEHICLE_SHEET = r"L:\Rollout\Vehicle Management - SC\2024 Vehicle Master.xlsx"
+
+# BEST PASS STUFF
+BEST_PASSS_TOLL_SHEET = r"L:\Rollout\Vehicle Management - SC\Best Pass\Statements - Invoices\2024\2024-08 Best Pass Toll Details.xlsx"
+BEST_PASS_FINANCIAL_SUMMARY_PDF = r"L:\Rollout\Vehicle Management - SC\Fuel Bills\2024\08 - August 2024\August-Financial Summary.pdf"
+BEST_PASS_PURCHASE_ACTIVITY_PDF = r"L:\Rollout\Vehicle Management - SC\Fuel Bills\2024\08 - August 2024\August-Purchase Activity.pdf"
+
+# MAN HOURS STUFF
+MAN_HOURS_LABOR_SHEET = r"L:\Rollout\Vehicle Management - SC\Fuel Bills\2024\08 - August 2024\08 - August 2024 Man Hours Labor.xlsx"
+LABOR_REPORT_DIRECTORY = (
+    r"L:\Rollout\Admin - FS\Admin\Admin Report\Hotels - Admin\Labor Reports"
+)
